5ab102401275f70d6baf62ee4e2561cef147a263
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-01-30 03:09:04 -0600

    Uploaded my project to git

---
+++ src/app.js
@@ -0,0 +1,54 @@
+
+
+let now = new Date();
+
+let timing = document.querySelector(".timing");
+
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let day = days[now.getDay()];
+
+timing.innerHTML = `${day} ${hours}:${minutes}`;
+
+
+
+function displayWeatherCondition(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+    document.querySelector("#temperature-description").innerHTML = response.data.weather[0].main;
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  let apiKey = "3dce9b1c66837262a25b3f448d354a76";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+
+
+ 
+  }
+  function searchLocation(position) {
+    let apiKey = "3dce9b1c66837262a25b3f448d354a76";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeatherCondition);
+    console.log(apiUrl);
+  
+  }
+
+  function getCurrentLocation (event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation)
+  }
+
+  let currentLocationButton = document.querySelector("#current-location-button");
+  currentLocationButton.addEventListener("click", getCurrentLocation);
+ 
+ let citySearch = document.querySelector("#search-city-button");
+ citySearch.addEventListener("click", search);
+
+
